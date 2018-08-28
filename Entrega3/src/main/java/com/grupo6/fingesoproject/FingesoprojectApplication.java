9b3a86bd84285d7083294215494339eed08475bf
Felipe González <felipe.gonzalez.me@usach.cl>
2018-08-28 13:50:59 -0300

    Javier empezamos a programar!! yay que divertidooooooo
    franco: te mandamos muchos amores. <3

---
+++ Entrega3/src/main/java/com/grupo6/fingesoproject/FingesoprojectApplication.java
@@ -0,0 +1,12 @@
+package com.grupo6.fingesoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingesoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingesoprojectApplication.class, args);
+    }
+}
